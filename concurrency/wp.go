681157a2c6d3bee10085365b1460743cb6526e2c
gallopelado <juanftp100@gmail.com>
2021-11-26 15:15:52 -0300

    add: implementacion de worker pools

---
+++ concurrency/wp.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	tasks := []int{2, 3, 4, 5, 7, 10, 12, 40, 50}
+	nWorkers := 3
+	// donde se van a leer los resultados
+	jobs := make(chan int, len(tasks))
+	// donde se van a transmitir los resultados
+	results := make(chan int, len(tasks))
+	for i := 0; i < nWorkers; i++ {
+		go Worker(i, jobs, results)
+	}
+	for _, value := range tasks {
+		jobs <- value
+	}
+	close(jobs)
+	for r := 0; r < len(tasks); r++ {
+		<-results
+	}
+}
+
+func Worker(id int, jobs <-chan int, results chan<- int) {
+	for job := range jobs {
+		fmt.Printf("Worker with id %d started fib with %d\n", id, job)
+		fib := Fibonacci(job)
+		fmt.Printf("Worker with id %d, job %d and fib %d\n", id, job, fib)
+		results <- fib
+	}
+}
+
+func Fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return Fibonacci(n-1) + Fibonacci(n-2)
+}
